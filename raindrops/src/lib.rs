67a76f1fa3de48115c69db22e894415d6b89de60
Venkatesh Pitta <venkateshpitta@gmail.com>
2018-05-30 18:00:49 +1000

    First iteration of raindrops

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,3 +1,17 @@
 pub fn raindrops(n: usize) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let mut out = String :: new();
+    
+    if n%3 == 0 {
+        out.push_str("Pling")
+    }
+    if n%5 == 0 {
+        out.push_str("Plang")
+    }
+    if n%7 == 0 {
+        out.push_str("Plong")
+    }
+    match out.is_empty() {
+        true => n.to_string(),
+        false => out,
+    }
 }
